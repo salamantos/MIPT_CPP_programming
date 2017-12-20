db3ee9095573d313632ddcdf2a6fcf9998ebbcdb
salamantos <m.b.volkov@yandex.ru>
2017-12-20 18:52:13 +0300

    Кewrote the program

--- 3module/dist_between_segments/main.cpp
+++ 3module/dist_between_segments/main.cpp
@@ -6,139 +6,127 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <assert.h>
 
 using std::cin;
 using std::cout;
 using std::setprecision;
 
 
-#define eps 1e-9
+#define eps (double)1e-11
 
 struct Point {
-    double _x = 0, _y = 0, _z = 0;
+    double x = 0, y = 0, z = 0;
 
-    Point( double x, double y, double z ) : _x( x ), _y( y ), _z( z ) {}
+    Point( double _x, double _y, double _z ) : x( _x ), y( _y ), z( _z ) {}
 
     Point() = default;
 };
 
 struct Vector {
-    double _x = 0;
-    double _y = 0;
-    double _z = 0;
+    double x = 0;
+    double y = 0;
+    double z = 0;
 
-    Vector( double x, double y, double z ) : _x( x ), _y( y ), _z( z ) {}
+    Vector( double _x, double _y, double _z ) : x( _x ), y( _y ), z( _z ) {}
 
-    Vector( const Point& start, const Point& finish ) : _x( finish._x - start._x ), _y( finish._y - start._y ),
-                                                        _z( finish._z - start._z ) {}
+    Vector( const Point& start, const Point& finish ) : x( finish.x - start.x ), y( finish.y - start.y ),
+                                                        z( finish.z - start.z ) {}
 
     Vector operator+( const Vector& other ) {
-        return Vector( this->_x + other._x, this->_y + other._y, this->_z + other._z );
+        return {this->x + other.x, this->y + other.y, this->z + other.z};
     }
 
     Vector operator*( double c ) {
-        return Vector( this->_x * c, this->_y * c, this->_z * c );
+        return {this->x * c, this->y * c, this->z * c};
+    }
+
+    double scalar_product( const Vector& u ) const {
+        const Vector v = *this;
+        return ((u).x * (v).x + (u).y * (v).y + (u).z * (v).z);
+
+    }
+
+    double vector_product( const Vector& v ) const {
+        const Vector u = *this;
+        return u.norm() * v.norm() - u.scalar_product( v ) * u.scalar_product( v );
+    }
+
+    double norm() const {
+        return this->scalar_product( *this );
     }
 };
 
-double scalar_product( const Vector& v, const Vector& u ) {
-    return ((u)._x * (v)._x + (u)._y * (v)._y + (u)._z * (v)._z);
-
-}
-
-double vector_product( const Vector& u, const Vector& v ) {
-    double u_norn = scalar_product( u, u );
-    double uXv = scalar_product( u, v );
-    double v_norm = scalar_product( v, v );
-    return u_norn * v_norm - uXv * uXv;
-}
-
-//void make_some_compares(){
-//
-//}
-
-double dist_between_segments( const Point& p1, const Point& p2, const Point& r1, const Point& r2 ) {
-    Vector u( p2, p1 );
-    Vector v( r2, r1 );
-    Vector w( p1, r1 );
-    double u_norn = scalar_product( u, u );
-    double uXv = scalar_product( u, v );
-    double v_norm = scalar_product( v, v );
-    double uXw = scalar_product( u, w );
-    double vXw = scalar_product( v, w );
-    double D = vector_product( u, v ); // vector product
-    double s_c, t_c; // coefficients from 0 to 1, describing segments
-    double s_normalised, t_normalised; // Normalized s and t - coordinates in sOt
-    double sD = D; // s_c = s_normalised / sD, default sD = D >= 0
-    double tD = D; // t_c = t_normalised / tD, default tD = D >= 0
-
-    // compute the line parameters of the two closest points
-    if (D < eps) { // the lines are almost parallel
-        s_normalised = 0.0;         // force using point P0 on segment S1
-        sD = 1.0;         // to prevent possible division by 0.0 later
-        t_normalised = vXw;
-        tD = v_norm;
+void dist_between_lines( const Vector& u, const Vector& v, const Vector& w, double& s_closest, double& t_closest ) {
+    // Если прямые параллельны
+    if (u.vector_product( v ) < eps) {
+        s_closest = 0.0;
+        t_closest = v.scalar_product( w ) / v.norm();
     } else {
-        // get the closest points on the infinite lines
-        s_normalised = (uXv * vXw - v_norm * uXw);
-        t_normalised = (u_norn * vXw - uXv * uXw);
-
-        // sc<0 => the s=0 edge is visible
-        if (s_normalised <= 0.0) {
-            s_normalised = 0.0;
-            t_normalised = vXw;
-            tD = v_norm;
-        } else if (s_normalised >= sD) {
-            // sc>1  => the s=1 edge is visible
-            s_normalised = sD;
-            t_normalised = vXw + uXv;
-            tD = v_norm;
-        }
+        s_closest = (u.scalar_product( v ) * v.scalar_product( w ) - v.norm() * u.scalar_product( w )) /
+                    (u.vector_product( v ));
+        t_closest = (u.norm() * v.scalar_product( w ) - u.scalar_product( v ) * u.scalar_product( w )) /
+                    (u.vector_product( v ));
     }
-    // tc<0 => the t=0 edge is visible
-    if (t_normalised <= 0.0) {
-        t_normalised = 0.0;
+}
 
-        if (-uXw <= 0.0)
-            s_normalised = 0.0;
-        else if (-uXw >= u_norn)
-            s_normalised = sD;
-        else {
-            s_normalised = -uXw;
-            sD = u_norn;
-        }
-    } else if (t_normalised >= tD) {
-        // tc > 1  => the t=1 edge is visible
-        t_normalised = tD;
+double dist_between_segments( const Point& P0, const Point& P1, const Point& R0, const Point& R1 ) {
+    Vector u( P1, P0 );
+    Vector v( R1, R0 );
+    Vector w( P0, R0 );
 
-        if ((-uXw + uXv) <= 0.0)
-            s_normalised = 0;
-        else if ((-uXw + uXv) >= u_norn)
-            s_normalised = sD;
-        else {
-            s_normalised = (-uXw + uXv);
-            sD = u_norn;
-        }
+    // Сначала считаем расстояние между бесконечными прямыми, вычисляя s_closest и t_closest
+    double s_closest = 0.0;
+    double t_closest = 0.0;
+    dist_between_lines( u, v, w, s_closest, t_closest );
+
+    if (s_closest <= 0.0 + eps) {
+        s_closest = 0.0;
+        t_closest = v.scalar_product( w ) / v.norm();
+    } else if (s_closest >= 1.0 - eps) {
+        s_closest = 1.0;
+        t_closest = (v.scalar_product( w ) + u.scalar_product( v )) / v.norm();
     }
 
-    s_c = (std::abs( s_normalised ) <= eps ? 0.0 : s_normalised / sD); // s_c = s_normalised / sD, default sD = D >= 0
-    t_c = (std::abs( t_normalised ) <= eps ? 0.0 : t_normalised / tD); // t_c = t_normalised / tD, default tD = D >= 0
+    if (t_closest <= 0.0 + eps) {
+        t_closest = 0.0;
 
-    // get the difference of the two closest points
-    Vector dP = w + (u * s_c) + (v * t_c * (-1));  // =  S1(sc) - S2(tc)
+        if (-u.scalar_product( w ) <= 0.0 + eps) {
+            s_closest = 0.0;
+        } else if (-u.scalar_product( w ) >= u.norm() - eps) {
+            s_closest = 1.0;
+        } else {
+            s_closest = -u.scalar_product( w ) / u.norm();
+        }
+    } else if (t_closest >= 1.0 - eps) {
+        t_closest = 1.0;
 
-    return sqrt( scalar_product( dP, dP ));   // return the closest distance
+        if ((-u.scalar_product( w ) + u.scalar_product( v )) <= 0.0 + eps) {
+            s_closest = 0.0;
+        } else if ((-u.scalar_product( w ) + u.scalar_product( v )) >= u.norm() - eps) {
+            s_closest = 1.0;
+        } else {
+            s_closest = (-u.scalar_product( w ) + u.scalar_product( v )) / u.norm();
+        }
+    }
+    
+    s_closest = (std::abs( s_closest ) <= eps ? 0.0 : s_closest);
+    t_closest = (std::abs( t_closest ) <= eps ? 0.0 : t_closest);
+
+    Vector dP = w + (u * s_closest) + (v * t_closest * (-1));
+
+    return std::sqrt( dP.norm());
 }
 
 int main() {
     Point p1, p2, r1, r2;
-    cin >> p1._x >> p1._y >> p1._z
-        >> p2._x >> p2._y >> p2._z
-        >> r1._x >> r1._y >> r1._z
-        >> r2._x >> r2._y >> r2._z;
+    cin >> p1.x >> p1.y >> p1.z
+        >> p2.x >> p2.y >> p2.z
+        >> r1.x >> r1.y >> r1.z
+        >> r2.x >> r2.y >> r2.z;
 
     cout << std::fixed;
-    cout << setprecision( 10 );
+    cout << setprecision( 8 );
 
     cout << dist_between_segments( p1, p2, r1, r2 );
 
